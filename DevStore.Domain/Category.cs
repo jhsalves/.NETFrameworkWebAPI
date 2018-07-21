1f49d720664803b72c8e7597466b74caa326e7d0
Jonathas <jhsalves@gmail.com>
2018-07-21 19:27:28 -0300

    Fix domain layer

---
+++ DevStore.Domain/Category.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DevStore.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public override string ToString() {
+            return this.Title;
+        }
+    }
+}
